9c8f9bbd49f309abd771098280c7f23c3ebe2ff2
Austin.Tech <Admoore1310@gmail.com>
2024-08-23 22:33:25 +0000

    imporved script

--- dice.py
+++ dice.py
@@ -1,5 +1,6 @@
 import random
 import inflect
+import ast
 from deep_translator import GoogleTranslator
 
 
@@ -7,40 +8,8 @@ language='en' #what is your language mate?
 #print(GoogleTranslator().get_supported_languages(as_dict=True))#un comment this to make it show supported langs
 
 total_sides = 20 #makes since?
-PossibleSen = [#God save me
-    'You rolled an ', 
-    'Congrats for the ', 
-    'Another one bites the dust, ', 
-    'You summoned a number from the depths of randomness! ', 
-    'The dice gods have spoken: ', 
-    'Hold your breath... it’s a ', 
-    'Your fate is sealed with a ', 
-    'Destiny decided: ', 
-    'Drumroll, please... ', 
-    'As luck would have it, you got a ', 
-    'This is the one! You rolled a ', 
-    'No turning back now, it’s a ', 
-    'Feel the suspense... ', 
-    'In a shocking twist of events, it’s a ', 
-    'The universe aligns for a ', 
-    'You’ve been chosen by the RNG deities! It’s a ', 
-    'Against all odds, it’s a ', 
-    'Brace yourself... ', 
-    'Prepare to be amazed by your roll of ', 
-    'The stars have aligned to give you a ', 
-    'And the number is... ', 
-    'Here comes the big one... ', 
-    'With a flick of the wrist, you rolled a ',
-    'And just like that, it’s a ', 
-    'The suspense is over, you rolled a ', 
-    'Your destiny reveals... ',
-    'You’ve rolled the mystical number of ', 
-    'God, Glory, and Dice, ',
-    'And You Won... a ',
-    'what do you get when you roll a dice? ',
-    'What do you like about this ',
-    '1+1=',
-]
+file.open('prompts.txt')
+PossibleSen = 
 s=len(PossibleSen)
 def n2t(num):
     return(inflect.engine().number_to_words(num))
