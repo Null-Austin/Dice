13ff4daef3777198a3182fbeadf04dcd9bb8ce85
Austin.Tech <Admoore1310@gmail.com>
2024-08-26 18:10:12 +0000

    updated- again

--- dice.py
+++ dice.py
@@ -1,23 +1,27 @@
 import random
 import inflect
 import ast
-from deep_translator import GoogleTranslator
+#from deep_translator import GoogleTranslator removed due to scripting conflict.
+from termcolor import colored, cprint
+from os import system, name
+
 
 
 language='en' #what is your language mate? 
 #print(GoogleTranslator().get_supported_languages(as_dict=True))#un comment this to make it show supported langs
 
 total_sides = 20 #makes since?
-file.open('prompts.txt')
-PossibleSen = 
+file=open('prompts.txt')
+PossibleSen = ast.literal_eval(file.read())
 s=len(PossibleSen)
 def n2t(num):
     return(inflect.engine().number_to_words(num))
-def trans(text):
-    if language != 'en':
-        return(GoogleTranslator(source='auto', target=language).translate(text))
+def clear():
+    if name == 'nt':
+        _ = system('cls')
     else:
-        return(text)
+        _ = system('clear')
 while(True):
     a=n2t(random.randrange(0,total_sides))
-    input(trans(PossibleSen[random.randrange(0,s)]+a))
+    clear()
+    input(colored(PossibleSen[random.randrange(0,s)],'blue')+colored(a,'yellow'))
--- p.py
+++
@@ -1,5 +0,0 @@
-from deep_translator import GoogleTranslator
-
-# Use any translator you like, in this example GoogleTranslator
-translated = GoogleTranslator(source='auto', target='es').translate("Hello!")  # output -> Weiter so, du bist großartig
-print(translated)
